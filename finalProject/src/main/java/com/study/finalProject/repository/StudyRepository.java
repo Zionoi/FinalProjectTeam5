c4faf65586de6ab730dde9e5552a582125729d16
ebe0025 <tjdgns3728@naver.com>
2024-10-22 15:37:59 +0900

    중첩 검색 기능 추가
    중첩 검색 기능 추가

--- finalProject/src/main/java/com/study/finalProject/repository/StudyRepository.java
+++ finalProject/src/main/java/com/study/finalProject/repository/StudyRepository.java
@@ -2,6 +2,7 @@ package com.study.finalProject.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.study.finalProject.domain.Study;
@@ -9,7 +10,7 @@ import com.study.finalProject.domain.Study;
 
 
 @Repository
-public interface StudyRepository extends JpaRepository<Study, Long> {
+public interface StudyRepository extends JpaRepository<Study, Long>, JpaSpecificationExecutor<Study> {
 
 	List<Study> findByStudyDescContainingOrModalityContainingOrPNameContaining(String studyDesc, String modality, String pName);
 
