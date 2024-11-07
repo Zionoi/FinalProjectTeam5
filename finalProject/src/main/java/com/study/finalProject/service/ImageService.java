16c6851c0bfdbb40d510e0488f6c97bdfb0adc43
Zionoi <rladygks2848@naver.com>
2024-11-07 10:29:01 +0900

    .

--- finalProject/src/main/java/com/study/finalProject/service/ImageService.java
+++ finalProject/src/main/java/com/study/finalProject/service/ImageService.java
@@ -13,7 +13,6 @@ import com.study.finalProject.repository.ImageRepository;
 public class ImageService {
 	@Autowired
 	ImageRepository imageRepository;
-	
 	// studyKey와 seriesKey로 이미지 목록 가져오기
 	public List<Image> getImagesByStudyKeyAndSeriesKey(Long studyKey, Long seriesKey) {
 		
