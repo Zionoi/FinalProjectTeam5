d4e886d07b45f6f9041cde9aee8e29c4305f019e
Zionoi <rladygks2848@naver.com>
2024-11-07 15:49:52 +0900

    .

--- finalProject/src/main/java/com/study/finalProject/controller/PatientController.java
+++ finalProject/src/main/java/com/study/finalProject/controller/PatientController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.study.finalProject.domain.Patient;
 import com.study.finalProject.service.PatientService;
