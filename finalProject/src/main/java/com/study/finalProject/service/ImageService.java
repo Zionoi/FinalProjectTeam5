0d99833e5769c6758f70f1a449057299b72dfdd3
iseul011 <kayira011@naver.com>
2024-11-12 16:36:30 +0900

    주석 수정중(엄이슬)

--- finalProject/src/main/java/com/study/finalProject/service/ImageService.java
+++ finalProject/src/main/java/com/study/finalProject/service/ImageService.java
@@ -1,5 +1,7 @@
 package com.study.finalProject.service;
 
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +21,27 @@ public class ImageService {
 		
 		return imageRepository.getImagesByStudyKeyAndSeriesKey(studyKey, seriesKey);
 	}
+	
+	// 다음 시리즈
+	public List<String> getNextSeriesImages(Long studyKey, Long seriesKey) {
+        List<Image> images = imageRepository.findNextSeriesImages(studyKey, seriesKey);
+        List<String> imagePaths = new ArrayList<>();
+        for (Image image : images) {
+            String fullPath = Paths.get(image.getPath(), image.getFName()).toString().replace("\\", "/");
+            imagePaths.add(fullPath);
+        }
+        return imagePaths;
+    }
+
+	// 이전 시리즈
+    public List<String> getPreviousSeriesImages(Long studyKey, Long seriesKey) {
+        List<Image> images = imageRepository.findPreviousSeriesImages(studyKey, seriesKey);
+        List<String> imagePaths = new ArrayList<>();
+        for (Image image : images) {
+            String fullPath = Paths.get(image.getPath(), image.getFName()).toString().replace("\\", "/");
+            imagePaths.add(fullPath);
+        }
+        return imagePaths;
+    }
 
 }
